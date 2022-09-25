45ff7cef297a698b2e35661c9bd63e2e45210bfd
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-25 14:01:41 -0700

    anothertask

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string.remove('c')
-    my_string.remove('C')
-    return my_string
+    new_string = []
+    for i in my_string:
+        if i == 'c' or i == 'C' or i == None:
+            print("", end="")
+        else:
+            new_string.append(i)
+    return ''.join(new_string)
 print(no_c("Best School"))
 print(no_c("Chicago"))
 print(no_c("C is fun!"))
