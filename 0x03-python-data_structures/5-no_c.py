445ce3053e76c87921d5577a01748c9411904180
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-25 14:03:16 -0700

    hotfix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,11 +2,8 @@
 def no_c(my_string):
     new_string = []
     for i in my_string:
-        if i == 'c' or i == 'C' or i == None:
+        if i == 'c' or i == 'C':
             print("", end="")
         else:
             new_string.append(i)
     return ''.join(new_string)
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
