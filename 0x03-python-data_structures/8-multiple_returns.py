38ac02feda3757dcd3b6e2f7131410f5d989ad10
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-27 09:30:23 -0700

    hotfix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     length = len(sentence)
     char = sentence[0]
     if sentence == "":
-        return 0, None
+        return (0, "None")
     return length, char
