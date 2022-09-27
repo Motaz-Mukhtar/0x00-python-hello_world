74be2285205ebeb6f4802275e5cc7cf8e04cb33d
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-27 09:32:41 -0700

    fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return None
     if isinstance(my_list, list):
         my_list.reverse()
