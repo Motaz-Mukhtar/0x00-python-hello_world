4d6b31f4615bb340976b43cebfccbda59881f8c5
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-28 16:03:49 -0700

    hotfix

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+num = 1
+length = len(sys.argv) - 1
+
+if length == 1:
+    print("{:d} argument:".format(length))
+elif len(sys.argv) == 0:
+    print("0 arguments.")
+else:
+    print("{:d} arguments:".format(length))
+
+for i in range(length):
+    print("{:d}: {}".format(num, sys.argv[num]))
+    num += 1
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+length = len(sys.argv)
+num_sum = 0
+i = 1
+
+while i < length:
+    num_sum += int(sys.argv[i])
+    i += 1
+print(num_sum)
+
