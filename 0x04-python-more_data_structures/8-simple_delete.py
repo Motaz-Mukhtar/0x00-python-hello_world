a0d3b69b29e7893955d78d2f10d523885b98aaa8
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-29 18:07:41 -0700

    FIXESS

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in my_list:
-        del my_list[key]
-    return my_list
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
