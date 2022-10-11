d195dce4a90058903ced1d2515ea9dce9a67261e
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-11 09:24:59 -0700

    DOC

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+""" Defines a square calss """
+
 
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    """ Represent square """
 
+    def __init__(self, size):
+        """ Initialize a square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
