eca2f413c30f9b9f195b4975085620436660bc84
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-11 14:43:20 -0700

    AndAnotherOne

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Define Square Class"""
+
+
+class Square:
+    """Initialize a New square class"""
+    def __init__(self, size=0):
+        """__init__ Function
+
+        Args:
+            size (int): The size of new Square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """retritive Size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initialize a new Square.
+
+
+        Args:
+            value (int): The size of new square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Are of the square
+
+
+        Return:
+            The Current square Area
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints in stdout the square with the character (#)"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(0, self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                    
+                print("")
+
