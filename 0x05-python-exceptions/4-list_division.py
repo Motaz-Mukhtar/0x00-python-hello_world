7d533d1fed0a7f09e8edcfa8899d7460c7046a1f
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-13 03:51:06 -0700

    Add Fifth Task

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-
+    result_list = []
+    for i in range(list_length):
+        try:
+            result_list.append(my_list_1[i] / my_list_2[i])
+        except IndexError:
+            print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+            result_list.append(0)
+        except TypeError:
+            result_list.append(0)
+            print("wrong type")
+    return result_list
