bbb64f4024ec8bc956f961b47eef5964ac092e6c
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-24 16:37:43 -0700

    AnotherOne

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creat Rectangle Class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class Inherted from BaseGeometry class"""
+    def __init__(self, width, height):
+        """initslize function
+
+            Args:
+                width (int): width of Rectangle
+                height (int) height of Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
