0f2fcea277c49617cb295cfd136826938fc2ff38
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-25 15:24:48 -0700

    AnotherOne

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define Student Class"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """instanlize attributes
+
+            Args:
+                first_name (str): name
+                last_name (str): name
+                age (int): number
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
