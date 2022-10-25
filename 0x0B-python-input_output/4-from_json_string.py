2828ebf7c479b41d73c1ff0bbbcff555119b1b3d
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-25 12:30:50 -0700

    AddTheFifthTask

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Returns an object represented by JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """Represented by JSON string
+
+        Args:
+            my_str (str): JSON string
+    """
+    return json.load(my_str)
