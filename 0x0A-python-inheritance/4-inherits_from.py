47a3359617e6a216ac758780480d1e6f0e3f0546
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-24 17:32:28 -0700

    ff

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,15 @@
 
 
 def inherits_from(obj, a_class):
+    """return True if obj inherited from a_class
+        Ohterwise False.
+
+        Args:
+            obj (any): object instance
+            a_class (class): Base Class
+        Return:
+            True if obj inherited from a_class
+    """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
