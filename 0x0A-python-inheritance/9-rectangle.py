50ee6de2cf6d09036dc0446c2c1dfc95bfa525c9
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-24 17:20:06 -0700

    HHHH

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -21,6 +21,6 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        String = "[" + str(self.__class__.__name__) + "]"
+        String = "[" + str(self.__class__.__name__) + "] "
         String += str(self.__width) + "/" + str(self.__height)
         return String
