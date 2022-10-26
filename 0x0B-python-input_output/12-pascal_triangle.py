148abe83fb042dcf368b12eee666e4871acf7db2
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-26 16:15:52 -0700

    Hot Fix in Task 7

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """adds all arguments to python list"""
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-myList = []
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    with open('add_item.json', encoding="utf-8") as myFile:
-        myList = json.loads(myFile.read())
+    myList = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    pass
+    myList = []
 
 for i in sys.argv:
     if i == sys.argv[0]:
@@ -19,4 +19,3 @@ for i in sys.argv:
     myList.append(i)
 
 save_to_json_file(myList, 'add_item.json')
-load_from_json_file('add_item.json')
