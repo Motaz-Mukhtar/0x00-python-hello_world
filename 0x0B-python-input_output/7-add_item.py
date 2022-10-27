a4723e701c3c47f519035a21923fe49e8ec7f404
Motaz-Mukhtar <motazksa2@gmail.com>
2022-10-26 18:35:44 -0700

    HotFix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""adds all arguments to JSON list"""
+# 7-add_item.py
+"""Add all arguments to a python list at JSON File"""
 import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
 try:
-    myList = load_from_json_file("add_item.json")
+    items = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    myList = []
+    items = []
 
-myList.extend(sys.argv[1:])
-save_to_json_file(myList, "add_item.jso")
+items.extend(sys.argv[1:])
+save_to_json_file(items, "add_item.json")
