efa53cdf5c7f16e6be49479ee4e17d6abb7b3de7
Motaz-Mukhtar <motazksa2@gmail.com>
2022-11-03 18:50:25 +0300

    finished from two tests files, One left

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -2,7 +2,8 @@
 # test_base.py
 """Test for the Base Class on Base.py File"""
 import sys
-sys.path.insert(1, "/root/alx-higher_level_programming/0x0C-python-almost_a_circle")
+import json
+# sys.path.insert(1, "C:/Users/admin/Desktop/taifoor jalon/alx-higher_level_programming/0x0C-python-almost_a_circle")
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -16,11 +17,35 @@ class TestBase(unittest.TestCase):
         self.assertAlmostEqual(b2.id, 2)
         self.assertAlmostEqual(b3.id, 12)
 
-   # def test_first_method(self):
-   #     r1 = Rectangle(5, 1, 2, 10)
-  #      dictionary = r1.to_dictionary()
- #       string = '[' + str(dictionary) + ']'
-#        self.assertEqual(Base.to_json_string([dictionary]), string)
+    def test_to_json_string_method(self):
+        r1 = Rectangle(5, 1, 2, 10)
+        r2 = Rectangle(2, 20)
+        dictionary1 = r1.to_dictionary()
+        dictionary2 = r2.to_dictionary()
+        string1 = json.dumps([dictionary1])
+        string2 = json.dumps([dictionary2])
+        self.assertEqual(Base.to_json_string([dictionary1]), string1)
+        self.assertEqual(Base.to_json_string([dictionary2]), string2)
+        self.assertEqual(Base.to_json_string([]), "[]")
+    
+    # def test_from_json_string(self):
+    #     r1 = Rectangle(5, 20, 10, 9)
+    #     r2 = Rectangle(10, 2, 2, 5)
+    #     list_of_obj = r1.to_dictionary()
+    #     string = json.dumps(list_of_obj)
+    #     self.assertEqual(Base.from_json_string(), list_of_obj)
+
+    def test_create_method(self):
+        r1 = Rectangle(5, 1, 2, 19)
+        dictionary = {"width": 5, "height": 1, "x": 2, "y": 19}
+        r2 = Base.create(r1, **dictionary)
+        self.assertEqual(r1, r2)
+
+    # def test_load_from_file_method(self):
+        
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
