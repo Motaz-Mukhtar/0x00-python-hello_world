8a74b60f8432fe6124bfc8baac695d027fd70dc4
Motaz-Mukhtar <motazksa2@gmail.com>
2022-11-04 14:34:28 -0700

    htoFix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Define Square Class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x=x, y=y, id=id)
 
