0d86a49c725d926ee1d50eeb4580b0ad1921b556
Motaz-Mukhtar <motazksa2@gmail.com>
2022-12-19 10:01:46 -0800

    Modify 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = process.argv.sort();
+const arr = process.argv.sort(function(a, b){ return a - b });
 
 if (arr.length <= 3) console.log(0);
 else console.log(arr[arr.length - 2]);
