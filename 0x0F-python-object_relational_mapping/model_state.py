e2992d3e95ad672c6fc6fcf72a7dd0ed461ba58e
Motaz-Mukhtar <motazksa2@gmail.com>
2022-12-22 14:05:32 -0800

    Add files

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Containes the class definition of a State and an instance 
+Base = declarative_base():
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """ Define State that inherits from Base
+
+        id (int): represents a column of an auto-generated,
+                    unique integer can't be null and is
+                    primary key
+        name (string): represents a column of a string
+                        maximum 128 characters and can't
+                        be null
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
