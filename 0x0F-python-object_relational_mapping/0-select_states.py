8232af79c7842cc49ed6f1c1682b399c45c469d6
Motaz-Mukhtar <motazksa2@gmail.com>
2022-12-22 03:48:36 -0800

    hotfix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
+
 
 db = MySQLdb.connec\
  t(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
