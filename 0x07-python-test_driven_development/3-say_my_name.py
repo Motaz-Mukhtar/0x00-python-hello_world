095610e632e9a78c30884d1cb847c5ba1d2269d9
Motaz-Mukhtar <motazksa2@gmail.com>
2022-12-29 11:36:48 -0800

    hotfix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,12 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """ Prints 'My name is <first name> <last name>
+
+        Raises:
+           TypeError: first_name and last_name must be
+                      string otherwise TypeError riased
+    """
     if type(first_name) == str and type(last_name) == str:
         print("My name is {} {}".format(first_name, last_name))
     elif type(first_name) != str:
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Prints a square with the character #."""
+
+
+def print_square(size):
+    if type(size) != int or (type(size) == float and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        for x in range(0, size):
+            print("#", end="")
+        print()
