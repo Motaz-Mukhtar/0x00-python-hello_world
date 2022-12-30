1e16df4ba890756392c34db0f5d868bfa9c42be3
Motaz-Mukhtar <motazksa2@gmail.com>
2022-12-30 01:10:21 -0800

    hotfix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-""" 
+""" Prints a text with the 2 new lines """
+
+
+def text_indentation(text):
+    """ Prints text and 2 new lines after each
+        of these characters . ? and :
+
+        Raises:
+            TypeError: must be string
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    my_list = text.split(" ")
+
+    for j in my_list:
+        if j.endswith(":") or j.endswith("?") or j.endswith("."):
+            print(j, end="")
+        else:
+            print(j, end=" ")
+        for char in j:
+            if char == '.' or char == '?' or char == ':':
+                print('\n')
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" max_integer fucntion Test """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class Test_max(unittest.TestCase):
+    """ Test class for max_integer fucntion """
+    def test_normal(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
