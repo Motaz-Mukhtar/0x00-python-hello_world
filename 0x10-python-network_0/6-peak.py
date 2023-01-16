bb02e7bf5edb269fbf81fc9f5d71627262ce1556
Motaz-Mukhtar <motazksa2@gmail.com>
2023-01-16 13:13:43 -0800

    Add the peak.py file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
 # Finds a peak in a list of unsorted integers.
+
+
+def find_peak(list_of_integer=[]):
+    my_list = list_of_integer
+    i = 0
+    length = len(list_of_integer)
+    if len(list_of_integer) == 0:
+        return None
+
+    while i < len(my_list) - 1:
+        j = 0
+        while j < len(my_list) - 1:
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+            j += 1
+        i += 1
+
+    if length % 2 == 1 or length == 1:
+        return list_of_integer[length - 1]
+    else:
+        if length % list_of_integer[length - 1] == 0:
+            return list_of_integer[length - 1]
+        else:
+            return list_of_integer[length - 2]
