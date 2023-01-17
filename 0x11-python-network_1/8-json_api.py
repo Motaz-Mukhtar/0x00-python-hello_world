2bb1f1a4706e75bb8953d537001fb1f852071781
Motaz-Mukhtar <motazksa2@gmail.com>
2023-01-17 12:46:51 -0800

    Another one

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,3 +6,19 @@
 """
 import sys
 import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    pload = {"q": letter}
+
+    req = requests.post("http://0.0.0.0:5000/search_user", data=pload)
+
+    try:
+        res = req.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+    except ValuError:
+        print("Not a valid JSON")
