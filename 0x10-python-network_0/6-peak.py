822a79cc1877b79d19cd087dc657905b7e2dc0c7
Motaz-Mukhtar <motazksa2@gmail.com>
2023-01-19 08:07:33 -0800

    Modify 6-peak.py file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,21 +4,11 @@
 
 def find_peak(list_of_integer=[]):
     """ Return the peak number in the list """
-    my_list = list_of_integer
+    my_list = list_of_integer.sort()
     i = 0
     length = len(list_of_integer)
     if len(list_of_integer) == 0:
         return None
-
-    while i < len(my_list) - 1:
-        j = 0
-        while j < len(my_list) - 1:
-            if my_list[j] > my_list[j + 1]:
-                temp = my_list[j]
-                my_list[j] = my_list[j + 1]
-                my_list[j + 1] = temp
-            j += 1
-        i += 1
 
     if length % 2 == 1 or length == 1:
         return list_of_integer[length - 1]
