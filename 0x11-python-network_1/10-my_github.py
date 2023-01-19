152b436faf43913cebd1c37d550a13aeb37f3484
Motaz-Mukhtar <motazksa2@gmail.com>
2023-01-19 04:14:01 -0800

    Modify file 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,3 +5,12 @@
 """
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+
+    data = req.json()
+    print(data.get("id"))
