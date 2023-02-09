14e919a4f0573e83de8875adc7ca3aeae567b5e1
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-09 12:09:42 -0800

    hotfixes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,6 +2,6 @@
 
 const request = require('request');
 
-request(`https://swapi-api.alx-tools.com/api.films/${process.argv[2]}`, (err, res, body) => {
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err, res, body) => {
   if (err) { console.log(err); } else { console.log(JSON.parse(body).title); }
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 let count = 0;
 
 request(`${process.argv[2]}`, (err, response, body) => {
-  body = body.json();
+  body = JSON.parse(body);
   if (err) { console.log(err); } else {
     for (const i in body.results) {
       count = body.results[i].characters.filter((ele) => {
