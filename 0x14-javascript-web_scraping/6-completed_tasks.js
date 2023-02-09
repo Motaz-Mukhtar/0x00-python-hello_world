db2ae7ebcdb22ce30a185dc5efc118473e9b4f53
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-09 13:42:57 -0800

    Append 6-completed_tasks.js file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+let count = 1;
+const tasksObject = {};
+
+request('process.argv[2]', (err, response, body) => {
+  if (err) { console.log(err); } else {
+    const arr = JSON.parse(body).filter((ele) => {
+      return ele.completed === true ? ele : null;
+    });
+
+    for (const i in arr) {
+      tasksObject[`${count}`] = arr[i].id;
+      count++;
+    }
+  }
+  console.log(tasksObject);
+});
