54169b1240afd7ce521c5f10e66b079897749c04
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-09 14:25:13 -0800

    Hot Fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-let count = 1;
 const tasksObject = {};
 
 request('process.argv[2]', (err, response, body) => {
   if (err) { console.log(err); } else {
-    const arr = JSON.parse(body).filter((ele) => {
-      return ele.completed === true ? ele : null;
+    JSON.parse(body).forEach((ele) => {
+      if (ele.completed && tasksObject[ele.userId] === undefined) {
+        tasksObject[ele.userId] = 1;
+      } else if (ele.completed) {
+        tasksObject[ele.userId] += 1;
+      }
     });
-
-    for (const i in arr) {
-      tasksObject[`${count}`] = arr[i].userId;
-      count++;
-    }
   }
   console.log(tasksObject);
 });
