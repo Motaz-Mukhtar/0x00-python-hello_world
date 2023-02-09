6c8d23eb3d368fb595ff959a3f0ef4520f53b5f7
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-09 12:03:02 -0800

    Add 3-starwars_title.js and 4-starwars_count.js, javascript file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(`https://swapi-api.alx-tools.com/api.films/${process.argv[2]}`, (err, res, body) => {
+  if (err) { console.log(err); } else { console.log(JSON.parse(body).title); }
+});
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+let count = 0;
+
+request(`${process.argv[2]}`, (err, response, body) => {
+  body = body.json();
+  if (err) { console.log(err); } else {
+    for (const i in body.results) {
+      count = body.results[i].characters.filter((ele) => {
+        return ele.endsWith('people/18/');
+      });
+    }
+  }
+  console.log(count.length);
+});
