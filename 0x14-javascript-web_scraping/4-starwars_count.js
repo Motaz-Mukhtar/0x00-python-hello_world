6e2f8b0ad6da29e696d5bccb5e0d97e63f0ab9bf
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-09 22:52:44 -0800

    hotFix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,13 @@
 
 const request = require('request');
 const count = [];
-const charaUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(`${process.argv[2]}`, (err, response, body) => {
   body = JSON.parse(body);
   if (err) { console.log(err); } else {
     for (const i in body.results) {
       body.results[i].characters.filter((ele) => {
-        return ele === charaUrl ? count.push(ele) : null;
+        return ele.endsWith('/people/18/') ? count.push(ele) : null;
       });
     }
   }
